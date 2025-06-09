35336466aa59f29692bdcc319a492c00b25b8c0f
vojtivajti <mishkapetran@yandex.ru>
2025-06-09 18:24:49 +0400

    Внесены изменения в Задание №1 и Задание №2

--- src/HW3_ConditionalOperator2/Main.java
+++ src/HW3_ConditionalOperator2/Main.java
@@ -44,11 +44,11 @@ public class Main {
         int clientOS2 = 0;
         int clientDeviceYear = 2014;
 
-        if (clientOS2 == 0 && clientDeviceYear >= 2015) {
+        if (clientOS2 == 0 && clientDeviceYear >= 2015){
             System.out.println("Установите версию приложения для iOS по ссылке");
         } else if (clientOS2 == 0 && clientDeviceYear < 2015){
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (clientOS2 == 1 && clientDeviceYear >= 2015) {
+        } else if (clientOS2 == 1 && clientDeviceYear >= 2015){
             System.out.println("Установите версию для Android по ссылке");
         } else if (clientOS2 == 1 && clientDeviceYear < 2015){
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
